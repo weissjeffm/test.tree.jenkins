899109c88e4ef54a45f97c3e11b03f25aa6704d1
Jeff Weiss <jweiss@redhat.com>
2012-03-13 16:57:28 -0400

    first commit

---
+++ project.clj
@@ -0,0 +1,5 @@
+(defproject test.tree.jenkins "0.1.0-SNAPSHOT"
+  :description "Some additional functionality on top of test.tree that makes web ui testing easier to do with Jenkins"
+  :dependencies [[org.clojure/clojure "1.3.0"]
+                 [test.tree "0.6.1-SNAPSHOT"]
+                 [fn.trace "1.3.2.0-SNAPSHOT"]])
---
+++ src/test/tree/jenkins.clj
@@ -0,0 +1,44 @@
+(ns test.tree.jenkins
+  (:require [test.tree :as tree]
+            [test.tree.reporter :as report])
+  (:use clojure.pprint
+        fn.trace))
+
+(def test-traces (ref {}))
+
+(defn wrap-tracing [runner]
+  (fn [test]
+    (binding [tracer (fn [_ value & [out?]]
+                       (dosync
+                        (alter test-traces update-in [test]
+                               (fn [v]
+                                 (conj (or v (with-meta [] {:log true})) [value out?])))))]
+      (let [result (runner test)]
+        (if (-> (:result result) (= :fail))
+          (assoc-in  result [:error :trace] (@test-traces test))
+          result)))))
+
+(defn run-suite [suite & [{:keys [to-trace do-not-trace syntax-highlight-url]
+                           :or {to-trace []
+                                do-not-trace []
+                                syntax-highlight-url "/shared/syntaxhighlighter/"}}]]
+  (with-redefs [tree/runner (-> tree/execute
+                               wrap-tracing
+                               tree/wrap-blockers
+                               tree/wrap-timer
+                               tree/wrap-data-driven)]
+    (binding [tracer (per-thread-tracer)
+              *print-level* 10
+              *print-length* 30 
+              *print-pprint-dispatch* log-dispatch
+              report/syntax-highlight (report/syntax-highlighter syntax-highlight-url)]
+      (dotrace (remove do-not-trace (all-fns to-trace)) 
+        (let [reports (tree/run-suite suite)]
+          (println "----- Blockers -----\n ")
+          (let [blockers (->> reports
+                            vals
+                            (mapcat #(get-in % [:report :blocked-by]))
+                            (filter #(not (nil? %)))
+                            frequencies)]
+            (pprint blockers)))))))
+
