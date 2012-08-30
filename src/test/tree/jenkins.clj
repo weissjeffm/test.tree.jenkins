aa7ba9349544ec65c53c3465fcd9724417749b50
Jeff Weiss <jweiss@redhat.com>
2012-08-30 11:40:24 -0400

    Allow functions to trace to be specified as a function rather than a static list

--- src/test/tree/jenkins.clj
+++ src/test/tree/jenkins.clj
@@ -33,10 +33,14 @@
 
 (defn run-suite
   "Run the suite with tracing, and output a testng result file with
-   syntaxhighlighted trace. Also print a report of blockers."
-  [suite & [{:keys [to-trace do-not-trace syntax-highlight-url]
-             :or {to-trace []
-                  do-not-trace #{}
+   syntaxhighlighted trace. Also print a report of blockers. You can
+   specify a list of namespaces and functions to trace (or not trace).
+   Alternatively a function that generates a list of functions to
+   trace (if both the list and function are specified, the list
+   wins)."
+  [suite & [{:keys [to-trace to-trace-fn do-not-trace
+                    syntax-highlight-url]
+             :or {do-not-trace #{}
                   syntax-highlight-url "/shared/syntaxhighlighter/"}}]]
   (with-redefs [tree/runner (-> tree/execute
                                wrap-tracing
@@ -49,7 +53,9 @@
               *print-right-margin* 150
               *print-miser-width* 120 
               report/syntax-highlight (report/syntax-highlighter syntax-highlight-url)]
-      (dotrace (remove do-not-trace (all-fns to-trace)) 
+      (dotrace (cond to-trace (remove do-not-trace (all-fns to-trace))
+                     to-trace-fn (to-trace-fn)
+                     :else []) 
         (let [reports (tree/run-suite suite)
               show-simple-test (fn [i]
                                  (if (:steps i)
